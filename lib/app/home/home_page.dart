61383cbb935b351555d2b32f9cac7a7308df2a43
Masahiro Aoki <rafe.kun@gmail.com>
2021-07-10 23:35:10 +0900

    add home page

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -6,8 +6,127 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       backgroundColor: OshigotoColors.primaryBlack,
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(height: screenHeight / 20),
+            _Header(),
+            const SizedBox(height: 20),
+            _Card(),
+            _Card(),
+            _Card(),
+            _Card(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 30.0),
+        child: Text(
+          'Find Your\nFlutter Job',
+          style: TextStyle(
+            color: OshigotoColors.primaryWhite,
+            fontSize: screenWidth / 13,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Card extends StatelessWidget {
+  const _Card({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 12,
+        top: 3,
+        right: 12,
+        bottom: 3,
+      ),
+      child: Card(
+        elevation: 5,
+        color: OshigotoColors.secondaryBlack,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const SizedBox(
+              height: 3,
+            ),
+            const ListTile(
+              leading: FlutterLogo(
+                size: 40,
+              ),
+              title: Text(
+                'Flutter + Firebaseエンジニア',
+                style: TextStyle(
+                  color: OshigotoColors.primaryWhite,
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Text(
+                '(株)アットゲーム',
+                style: TextStyle(
+                  color: OshigotoColors.primaryGray,
+                  fontSize: 13,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 30, bottom: 12),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        elevation: 0, primary: OshigotoColors.secondaryGrey),
+                    onPressed: () {},
+                    child: Text('副業'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, bottom: 12),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        elevation: 0, primary: OshigotoColors.secondaryGrey),
+                    onPressed: () {},
+                    child: Text('1年~'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12, bottom: 12),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        elevation: 0, primary: OshigotoColors.secondaryGrey),
+                    onPressed: () {},
+                    child: Text('フルリモート'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/oshigoto_colors.dart
+++ lib/oshigoto_colors.dart
@@ -5,4 +5,7 @@ class OshigotoColors {
   static const primaryBlue = Color(0xFF503FF4);
   static const primaryWhite = Color(0xFFEAEAEC);
   static const primaryGray = Color(0xFF48474D);
+
+  static const secondaryBlack = Color(0xFF1C1C24);
+  static const secondaryGrey = Color(0xFF282931);
 }
